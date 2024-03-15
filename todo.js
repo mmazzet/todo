d233af153fa1c86312fec94e4f48c711a54b71d2
Marianna Mazzette <20104120@mail.wit.ie>
2024-03-15 21:55:01 +0000

    first commit

---
+++ todo.js
@@ -0,0 +1,20 @@
+let todoItems = [];
+
+function renderTodo(todo) {
+  const table = document.getElementById("todo-table");
+  const row = table.insertRow(-1);
+  const textCell = row.insertCell(0);
+  textCell.innerText = todo.text;
+  const dateCell = row.insertCell(1);
+  dateCell.innerText = todo.date;
+}
+
+function addTodo() {
+  const todoText = document.getElementById("todo-id").value;
+  const todo = {
+    text: todoText,
+    date: new Date().toLocaleString("en-IE")
+  };
+  todoItems.push(todo);
+  renderTodo(todo);
+}
