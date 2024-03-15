422b20b26f068b62dbbc508f5af8b87467162d5d
Marianna Mazzette <20104120@mail.wit.ie>
2024-03-15 22:26:07 +0000

    added uuidv4

--- todo.js
+++ todo.js
@@ -18,3 +18,10 @@ function addTodo() {
   todoItems.push(todo);
   renderTodo(todo);
 }
+
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
